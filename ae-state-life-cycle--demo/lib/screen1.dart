372974bcde27d4d334e32595ffef9baf46be1463
Robbin Law <robbinl@telusplanet.net>
2019-10-21 18:53:11 -0600

    a

--- ae-state-life-cycle--demo/lib/screen1.dart
+++ ae-state-life-cycle--demo/lib/screen1.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'screen2.dart';
 
-class Screen1 extends StatelessWidget {
+class Screen1 extends StatefulWidget {
+  @override
+  _Screen1State createState() => _Screen1State();
+}
+
+class _Screen1State extends State<Screen1> {
+  @override
+  void initState() {
+    super.initState();
+    print('Screen1 initState Called');
+  }
+  @override
+  void deactivate(){
+    super.deactivate();
+    print('Screen1 deactivate Called');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('Screen1 build Called');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
