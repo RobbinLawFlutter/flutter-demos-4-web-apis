9c95d4d7ad19ae484dc06aef89d0c3a6b617b6e7
Robbin Law <robbinl@telusplanet.net>
2019-11-04 18:59:46 -0700

    a

--- ac_location/lib/screens/loading_screen.dart
+++ ac_location/lib/screens/loading_screen.dart
@@ -12,9 +12,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   void initState() {
-    getLocation();
-    // TODO: implement initState
     super.initState();
+    getLocation();
   }
 
   void getLocation() async {
@@ -25,10 +24,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=53.5461&lon=113.4938&appid=b6907d289e10d714a6e88b30761fae22');
+    print(response.statusCode);
     if (response.statusCode == 200){
       String data = response.body;
       print(data);
+      var latitude = jsonDecode(data)['coord']['lat'];
+      print(latitude);
       var longitude = jsonDecode(data)['coord']['lon'];
       print(longitude);
       var weatherDescription = jsonDecode(data)['weather'][0]['description'];
