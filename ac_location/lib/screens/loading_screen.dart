529d8a2ce7b7750529ae098dcb37994daad3d160
Robbin Law <robbinl@telusplanet.net>
2019-11-06 16:58:10 -0700

    a

--- ac_location/lib/screens/loading_screen.dart
+++ ac_location/lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:clima/screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -47,6 +48,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: Center(child: SpinKitDoubleBounce(color: Colors.white, size: 100,),),);
   }
 }
