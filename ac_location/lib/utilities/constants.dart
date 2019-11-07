e58d2a5f507b3c73c94a4103e7ae950272fa458e
Robbin Law <robbinl@telusplanet.net>
2019-11-06 18:34:19 -0700

    a

--- ac_location/lib/utilities/constants.dart
+++ ac_location/lib/utilities/constants.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 
 const kTempTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
-  fontSize: 100.0,
+  fontSize: 70.0,
 );
 
 const kMessageTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
-  fontSize: 60.0,
+  fontSize: 50.0,
 );
 
 const kButtonTextStyle = TextStyle(
-  fontSize: 30.0,
+  fontSize: 20.0,
   fontFamily: 'Spartan MB',
 );
 
