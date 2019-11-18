bc05aaa88133d0b65ea915b8895d00ab01c3e36d
Robbin <robbinl@telusplanet.net>
2019-11-18 16:12:54 -0700

    a

--- flash_chat_again/lib/main.dart
+++ flash_chat_again/lib/main.dart
@@ -4,7 +4,9 @@ import 'package:flash_chat_again/screens/login_screen.dart';
 import 'package:flash_chat_again/screens/registration_screen.dart';
 import 'package:flash_chat_again/screens/chat_screen.dart';
 
-//import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 void main() => runApp(FlashChat());
 
