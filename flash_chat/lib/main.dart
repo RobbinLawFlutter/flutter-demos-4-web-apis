746ee21b5998395394c4ab1df1a31f7b3e81097e
Robbin <robbinl@telusplanet.net>
2019-11-18 15:18:26 -0700

    a

--- flash_chat/lib/main.dart
+++ flash_chat/lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
