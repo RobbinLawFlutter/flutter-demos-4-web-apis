4f1bb396f52a3622bd347f5100edca295af89481
Robbin Law <robbinl@telusplanet.net>
2020-06-29 15:37:04 -0600

    a

--- ac_web-services-WeatherApp/ios/Flutter/flutter_export_environment.sh
+++ ac_web-services-WeatherApp/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\robbinl\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\robbinl\Desktop\flutter\flutter-intermediate-1\ac_web-services-WeatherApp"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\robbi\OneDrive\Desktop\flutter\flutter-intermediate\ac_web-services-WeatherApp"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\robbinl\src\flutter\bin\cache\artifacts\engine\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
