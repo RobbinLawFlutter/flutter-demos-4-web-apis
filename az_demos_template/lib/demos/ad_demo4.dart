11696e11c010835412b96799db5a5d08ff8b0690
robbinl <robbinl@telusplanet.net>
2020-11-03 10:41:35 -0700

    a

---
+++ az_demos_template/lib/demos/ad_demo4.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Demo4 extends StatelessWidget {
+  Demo4({this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: MyDemo(),
+    );
+  }
+}
+
+class MyDemo extends StatefulWidget {
+  @override
+  _MyDemoState createState() => _MyDemoState();
+}
+
+class _MyDemoState extends State<MyDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+}
