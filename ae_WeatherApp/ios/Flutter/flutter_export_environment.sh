f893c8af269524f3079ce9f88bb23e36ab30b85c
robbinl <robbinl@telusplanet.net>
2020-11-15 17:14:56 -0700

    a

--- ae_WeatherApp/ios/Flutter/flutter_export_environment.sh
+++ ae_WeatherApp/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\FlutterDev\FlutterSDK\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\FlutterDev\MyAppRepos\flutter-intermediate\ac_web-services-WeatherApp"
+export "FLUTTER_APPLICATION_PATH=C:\FlutterDev\MyAppRepos\flutter-intermediate\ae_WeatherApp"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
