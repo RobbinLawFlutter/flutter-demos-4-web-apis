24fffdd731d81458a1456854ce786bdb8722b344
robbinl <robbinl@telusplanet.net>
2020-11-27 18:13:48 -0700

    a

--- ap_firebase_demos/lib/main.dart
+++ ap_firebase_demos/lib/main.dart
@@ -7,39 +7,11 @@ import 'package:cloud_firestore/cloud_firestore.dart' as firestore;
 import 'package:ap_firebase_demos/dummydata.dart' as dummydata;
 import 'package:ap_firebase_demos/firebasedemo.dart' as firebasedemo;
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await firebase.Firebase.initializeApp();
   runApp(MyApp());
 }
-
-// class App extends StatelessWidget {
-//   // Create the initialization Future outside of `build`:
-//   final Future<firebase.FirebaseApp> _initialization =
-//       firebase.Firebase.initializeApp();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return FutureBuilder(
-//       // Initialize FlutterFire:
-//       future: _initialization,
-//       builder: (context, snapshot) {
-//         // Check for errors
-//         if (snapshot.hasError) {
-//           return SomethingWentWrong();
-//         }
-
-//         // Once complete, show your application
-//         if (snapshot.connectionState == ConnectionState.done) {
-//           return MyApp();
-//         }
-
-//         // Otherwise, show something whilst waiting for initialization to complete
-//         return Loading();
-//       },
-//     );
-//   }
-// }
 
 class MyApp extends StatelessWidget {
   @override
@@ -48,32 +20,6 @@ class MyApp extends StatelessWidget {
       title: 'Baby Names',
       //home: dummydata.MyHomePage(),
       home: firebasedemo.MyHomePage(),
-    );
-  }
-}
-
-class SomethingWentWrong extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: Text(
-        'opps',
-        textDirection: TextDirection.ltr,
-      )),
-    );
-  }
-}
-
-class Loading extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: Text(
-        'opps',
-        textDirection: TextDirection.ltr,
-      )),
     );
   }
 }
