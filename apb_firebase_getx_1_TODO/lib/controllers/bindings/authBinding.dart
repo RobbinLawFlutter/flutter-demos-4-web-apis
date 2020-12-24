8f7e37d85e6590976bbfcf51e7bfdd105423bf29
robbinl <robbinl@telusplanet.net>
2020-12-24 16:04:29 -0700

    a

--- apb_firebase_getx_1_TODO/lib/controllers/bindings/authBinding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:robbinlaw/controllers/authController.dart';
-
-class AuthBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.put<AuthController>(AuthController(), permanent: true);
-  }
-}
