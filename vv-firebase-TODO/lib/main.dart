c8b40c17d7cfd8f4aad6a2965bdc7d3924f055ff
robbinl <robbinl@telusplanet.net>
2021-02-07 09:45:20 -0700

    add some vids

--- vv-firebase-TODO/lib/main.dart
+++ vv-firebase-TODO/lib/main.dart
@@ -1,3 +1,9 @@
+//Isolates, Futures, Streams, async/await articles from medium
+//https://medium.com/dartlang/dart-asynchronous-programming-isolates-and-event-loops-bffc3e296a6a
+
+//Firestore by the flutter in focus series
+//https://www.youtube.com/watch?v=DqJ_KjFzL9I&list=PLjxrf2q8roU2HdJQDjJzOeO6J3FoFLWr2&index=9
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart' as firebase;
