04529ebaa8ebfe1ffe1bf12faf4129d5517088c2
robbinl <robbinl@telusplanet.net>
2021-03-15 15:05:20 -0600

    a

--- aa-maps/lib/demos/ab-demo2.dart
+++ aa-maps/lib/demos/ab-demo2.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
-class Customer {
-  Customer(this.name, this.age);
+class CustomerOne {
+  CustomerOne(this.name, this.age);
   String name;
   int age;
+
+  @override
+  String toString() {
+    return '{ ${this.name}, ${this.age} }';
+  }
+}
+
+class CustomerTwo {
+  CustomerTwo({this.name, this.age});
+  String name;
+  int age;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'age': age,
+    };
+  }
 
   @override
   String toString() {
@@ -46,24 +64,24 @@ void performTasks() {
   Map map = {'Jack': 23, 'Adam': 27, 'Katherin': 25};
   //Map map0 = {'name': 'Jack', 'age': 27};
   var list1 = [];
-  //Convert a map to a list of Customer objects.
-  map.forEach((k, v) => list1.add(Customer(k, v)));
+  //Convert a map to a list of CustomerOne objects.
+  map.forEach((k, v) => list1.add(CustomerOne(k, v)));
   print('list1= $list1');
-  //Another way to convert a map to a list of Customer objects.
+  //Another way to convert a map to a list of CustomerOne objects.
   var list2 = [];
-  map.entries.forEach((e) => list2.add(Customer(e.key, e.value)));
+  map.entries.forEach((e) => list2.add(CustomerOne(e.key, e.value)));
   print('list2= $list2');
-  //Another way to convert a map to a list of Customer objects.
+  //Another way to convert a map to a list of CustomerOne objects.
   //We convert the map into a view and then to a list.
   var list3 = [];
-  list3 = map.entries.map((e) => Customer(e.key, e.value)).toList();
+  list3 = map.entries.map((e) => CustomerOne(e.key, e.value)).toList();
   print('list3= $list3');
 
-  //Creating a list of Customer objects.
+  //Creating a list of CustomerOne objects.
   List list4 = [];
-  list4.add(Customer('Jack', 23));
-  list4.add(Customer('Adam', 27));
-  list4.add(Customer('Katherin', 25));
+  list4.add(CustomerOne('Jack', 23));
+  list4.add(CustomerOne('Adam', 27));
+  list4.add(CustomerOne('Katherin', 25));
   //Converting a list to a map.
   var map1 = Map.fromIterable(list4, key: (e) => e.name, value: (e) => e.age);
   print('map1= $map1');
@@ -83,12 +101,38 @@ void performTasks() {
     {'name': 'Adam', 'age': 27},
     {'name': 'katie', 'age': 25},
   ];
-  // Convert the List<Map<String, dynamic> into a List<Customer>.
+  // Convert the List<Map<String, dynamic> into a List<CustomerOne>.
   var list5 = List.generate(myListOfMaps.length, (i) {
-    return Customer(
+    return CustomerOne(
       myListOfMaps[i]['name'],
       myListOfMaps[i]['age'],
     );
   });
   print('list5= $list5');
+
+  // //Creating a list of CustomerTwo objects.
+  List listOfCustomerObjects1 = [];
+  listOfCustomerObjects1.add(CustomerTwo(name: 'Jack', age: 23));
+  listOfCustomerObjects1.add(CustomerTwo(name: 'Adam', age: 27));
+  listOfCustomerObjects1.add(CustomerTwo(name: 'Katherin', age: 25));
+  //Convert the List<CustomerTwo> into a List<Map<String, dynamic>>
+  var listOfMaps1 = List.generate(listOfCustomerObjects1.length, (i) {
+    return listOfCustomerObjects1[i].toMap();
+  });
+  print('listOfMaps1= $listOfMaps1');
+
+  //Creating a list of maps.
+  List<Map<String, dynamic>> listOfMaps2 = [
+    {'name': 'Jack', 'age': 23},
+    {'name': 'Adam', 'age': 27},
+    {'name': 'katie', 'age': 25},
+  ];
+  // Convert the List<Map<String, dynamic>> into a List<CustomerTWo>.
+  var listOfCustomerObjects2 = List.generate(listOfMaps2.length, (i) {
+    return CustomerTwo(
+      name: listOfMaps2[i]['name'],
+      age: listOfMaps2[i]['age'],
+    );
+  });
+  print('listOfCustomerObjects2= $listOfCustomerObjects2');
 }
--- aa-maps/lib/main.dart
+++ aa-maps/lib/main.dart
@@ -1,12 +1,3 @@
-//Isolates and the Event Loop from Google Flutter in Focus series
-//https://www.youtube.com/watch?v=vl_AaCgudcY&list=PLjxrf2q8roU2HdJQDjJzOeO6J3FoFLWr2&index=14
-
-//Futures from Google Flutter in Focus series 8min.
-//https://www.youtube.com/watch?v=OTS-ap9_aXc&list=PLjxrf2q8roU2HdJQDjJzOeO6J3FoFLWr2&index=15
-
-//Async Await from Google Flutter in Focus series 8min.
-//https://www.youtube.com/watch?v=SmTCmDMi4BY&list=PLjxrf2q8roU2HdJQDjJzOeO6J3FoFLWr2&index=17
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
