0ec88c8618f71fe871f5c2582525e0b0ee2b37e6
robbinl <robbinl@telusplanet.net>
2021-03-15 14:17:32 -0600

    a

---
+++ bb-futures-async-await/lib/main.dart
@@ -0,0 +1,37 @@
+//Isolates and the Event Loop from Google Flutter in Focus series
+//https://www.youtube.com/watch?v=vl_AaCgudcY&list=PLjxrf2q8roU2HdJQDjJzOeO6J3FoFLWr2&index=14
+
+//Futures from Google Flutter in Focus series 8min.
+//https://www.youtube.com/watch?v=OTS-ap9_aXc&list=PLjxrf2q8roU2HdJQDjJzOeO6J3FoFLWr2&index=15
+
+//Async Await from Google Flutter in Focus series 8min.
+//https://www.youtube.com/watch?v=SmTCmDMi4BY&list=PLjxrf2q8roU2HdJQDjJzOeO6J3FoFLWr2&index=17
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:robbinlaw/demos/aa-demo1.dart';
+import 'package:robbinlaw/demos/ab-demo2.dart';
+import 'package:robbinlaw/demos/ac-demo3.dart';
+import 'package:robbinlaw/demos/ad-demo4.dart';
+
+import 'package:robbinlaw/views/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Demo1(),
+      //home: Demo2(),
+      //home: Demo3(),
+      //home: Demo4(),
+      //home: HomeView(),
+    );
+  }
+}
