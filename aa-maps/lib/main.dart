945a8aa70d38a6538373b1525a78304167293ba6
robbinl <robbinl@telusplanet.net>
2021-03-15 13:06:23 -0600

    a

--- aa-maps/lib/main.dart
+++ aa-maps/lib/main.dart
@@ -1,6 +1,20 @@
+//Isolates and the Event Loop from Google Flutter in Focus series
+//https://www.youtube.com/watch?v=vl_AaCgudcY&list=PLjxrf2q8roU2HdJQDjJzOeO6J3FoFLWr2&index=14
+
+//Futures from Google Flutter in Focus series 8min.
+//https://www.youtube.com/watch?v=OTS-ap9_aXc&list=PLjxrf2q8roU2HdJQDjJzOeO6J3FoFLWr2&index=15
+
+//Async Await from Google Flutter in Focus series 8min.
+//https://www.youtube.com/watch?v=SmTCmDMi4BY&list=PLjxrf2q8roU2HdJQDjJzOeO6J3FoFLWr2&index=17
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:robbinlaw/bindings/globalBindings.dart';
+
+import 'package:robbinlaw/demos/aa_demo1.dart';
+import 'package:robbinlaw/demos/ab_demo2.dart';
+import 'package:robbinlaw/demos/ac_demo3.dart';
+import 'package:robbinlaw/demos/ad_demo4.dart';
+
 import 'package:robbinlaw/views/home.dart';
 
 void main() {
@@ -12,9 +26,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialBinding: GlobalBindings(),
-      home: HomeView(),
       theme: ThemeData.dark(),
+      home: Demo1(),
+      //home: Demo2(),
+      //home: Demo3(),
+      //home: Demo4(),
+      //home: HomeView(),
     );
   }
 }
--- aa-maps/lib/models/numberModel.dart
+++
@@ -1,7 +0,0 @@
-class NumberModel {
-  NumberModel({this.type, this.title, this.image});
-
-  final String type;
-  final String title;
-  final String image;
-}
