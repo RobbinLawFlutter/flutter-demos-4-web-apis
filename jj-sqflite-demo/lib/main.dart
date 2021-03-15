5a17f2636c9bbee150f902eb70822f14c4ac6695
robbinl <robbinl@telusplanet.net>
2021-03-15 16:21:32 -0600

    a

---
+++ jj-sqflite-demo/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:robbinlaw/demos/aa-demo1.dart';
+import 'package:robbinlaw/demos/ab-demo2.dart';
+import 'package:robbinlaw/demos/ac-demo3.dart';
+import 'package:robbinlaw/demos/ad-demo4.dart';
+
+import 'package:robbinlaw/views/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Demo1(),
+      //home: Demo2(),
+      //home: Demo3(),
+      //home: Demo4(),
+      //home: HomeView(),
+    );
+  }
+}
---
+++ jj-sqflite-demo/lib/views/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text('hi'),
+        ],
+      ),
+    );
+  }
+}
