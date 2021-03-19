72dbe853f205852b978de3a8f008058b7a106bc8
robbinl <robbinl@telusplanet.net>
2021-03-19 17:01:50 -0600

    a

--- bb-futures-async-await/lib/demos/aa-demo1.dart
+++ bb-futures-async-await/lib/demos/aa-demo1.dart
@@ -25,82 +25,91 @@ class _MyDemoState extends State<MyDemo> {
       child: RaisedButton(
         child: Text('Press'),
         onPressed: () {
-          performTasks();
+          //performTasks1();
+          //performTasks2();
+          performTasks3();
         },
       ),
     );
   }
 }
 
-void performTasks() {
-  //task1();
-  //task2();
-  //task2a();
-  //task2b();
-  //String task2Result = task2c();
-  //task3();
-  //task3a(task2Result);
+void performTasks1() {
+  task1();
+  task2();
 }
 
-void task1() {
-  print('Task 1 start');
-  String result = 'task 1 data';
-  print('Task 1 complete');
+void performTasks2() {
+  String task3Result = task3();
+  task4(task3Result);
 }
 
-void task2() {
-  print('Task 2 start');
-  String result = 'task 2 data';
-  print('Task 2 complete');
+void performTasks3() async {
+  String task5Result = await task5();
+  task6(task5Result);
 }
 
 //In this method the sleep method runs synchronously.
-void task2a() {
-  print('Task 2a start');
-  Duration myThreeSeconds = Duration(seconds: 3);
+void task1() {
+  print('Task 1 start');
+  Duration myFiveSeconds = Duration(seconds: 5);
   //We can access sleep() by importing dart.io.
-  sleep(myThreeSeconds);
-
-  String result = 'task 2a data';
-  print('Task 2a complete');
+  sleep(myFiveSeconds);
+  print('Task 1 end');
 }
 
 //In this method the Future.delayed method runs asynchronously.
-void task2b() {
-  print('Task 2b start');
+void task2() {
+  print('Task 2 start');
   Duration myFiveSeconds = Duration(seconds: 5);
   //async method that will delay for 5 seconds,
   //and then run the callback method, which is
   //the second parm.
   Future.delayed(myFiveSeconds, () {
-    String result = 'task 2b data';
-    print('Task 2b complete');
+    print('Task 2 future callback complete');
   });
-  print('Task 2b waiting for 5 seconds');
+  print('Task 2 end');
 }
 
-String task2c() {
-  print('Task 2c start');
+//In this method the Future.delayed method runs asynchronously.
+String task3() {
+  print('Task 3 start');
   String result;
   Duration myFiveSeconds = Duration(seconds: 5);
   //async method that will delay for 5 seconds,
   //and then run the callback method, which is
   //the second parm.
   Future.delayed(myFiveSeconds, () {
-    result = 'task 2c data';
-    print('Task 2c complete');
+    result = 'task 3 data';
+    print('Task 3 future callback complete');
   });
+  print('Task 3 end');
   return result;
 }
 
-void task3() {
-  print('Task 3 start');
-  String result = 'task 3 data';
-  print('Task 3 complete');
+void task4(String data) {
+  print('Task 4 start');
+  print('Task 4 end with $data');
 }
 
-void task3a(String taskTwoData) {
-  print('Task 3a start');
-  String result = 'task 3a data';
-  print('Task 3a complete with $taskTwoData');
+//In this method the Future.delayed method runs synchronously.
+Future<String> task5() async {
+  print('Task 5 start');
+  String result;
+  Duration myFiveSeconds = Duration(seconds: 5);
+  //async method that will delay for 5 seconds,
+  //and then run the callback method, which is
+  //the second parm.
+  //Using await will cause this to now act synchronously.
+  await Future.delayed(myFiveSeconds, () {
+    result = 'task 5 data';
+    print('Task 5 future callback complete');
+  });
+  print('Task 5 end');
+  return result;
+}
+
+void task6(String data) {
+  print('Task 6 start');
+  print('Task 6 end with $data');
 }
--- bb-futures-async-await/lib/demos/ab-demo2.dart
+++ bb-futures-async-await/lib/demos/ab-demo2.dart
@@ -31,35 +31,4 @@ class _MyDemoState extends State<MyDemo> {
   }
 }
 
-void performTasks() async {
-  task1();
-  String task2Result = await task2c();
-  task3a(task2Result);
-}
-
-void task1() {
-  print('Task 1 start');
-  String result = 'task 1 data';
-  print('Task 1 complete');
-}
-
-Future<String> task2c() async {
-  print('Task 2 start');
-  String result;
-  Duration myFiveSeconds = Duration(seconds: 5);
-  //async method that will delay for 5 seconds,
-  //and then run the callback method, which is
-  //the second parm.
-  //Using await will cause this to now act synchronously.
-  await Future.delayed(myFiveSeconds, () {
-    result = 'task 2 data';
-    print('Task 2 complete');
-  });
-  return result;
-}
-
-void task3a(String taskTwoData) {
-  print('Task 3 start');
-  String result = 'task 3 data';
-  print('Task 3 complete with $taskTwoData');
-}
+void performTasks() {}
