14f6ccd73f37bc140fc4dc05bb24e020018b9449
robbinl <robbinl@telusplanet.net>
2021-04-07 13:41:26 -0600

    a

--- tt-firebase-demos/lib/firestore_demo1.dart
+++ tt-firebase-demos/lib/firestore_demo1.dart
@@ -1,108 +1,85 @@
-//This demo is from the following codelab.
-//https://codelabs.developers.google.com/codelabs/flutter-firebase#1
-
-//Intro to FireStore from Google Futter in Focus series
-//https://www.youtube.com/watch?v=DqJ_KjFzL9I&t=7s
-
-//What is a nosql database... 12 videos about FireStore
-//https://www.youtube.com/watch?v=v_hR4K4auoQ&list=PLl-K7zZEsYLluG5MCVEzXAQ7ACZBCuZgZ
-
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart' as firestore;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() {
-    return _MyHomePageState();
-  }
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Baby Name Votes')),
-      body: _buildBody(context),
-    );
-  }
-
-  Widget _buildBody(BuildContext context) {
-    //streams from Google Flutter in Focus series.
-    //https://www.youtube.com/watch?v=nQBpOIHE4eE&list=PLjxrf2q8roU2HdJQDjJzOeO6J3FoFLWr2&index=16
-
-    return StreamBuilder<firestore.QuerySnapshot>(
-      stream:
-          firestore.FirebaseFirestore.instance.collection('baby').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
-        return _buildList(context, snapshot.data.docs);
-      },
-    );
-  }
-
-  Widget _buildList(
-      BuildContext context, List<firestore.DocumentSnapshot> snapshots) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: snapshots
-          .map((snapshot) => _buildListItem(context, snapshot))
-          .toList(),
-    );
-  }
-
-  Widget _buildListItem(
-      BuildContext context, firestore.DocumentSnapshot snapshot) {
-    final record = Record.fromSnapshot(snapshot);
-
-    return Padding(
-      //key: ValueKey(record.name),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        //ListTile can act as a button with onTap property.
-        child: ListTile(
-            title: Text(record.name),
-            trailing: Text(record.votes.toString()),
-            onTap: () {
-              print(record);
-              record.reference
-                  .update({'votes': firestore.FieldValue.increment(1)});
-            }),
+      appBar: AppBar(
+        title: Text("Flutter Firestore Demo 2"),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                child: Text("Create"),
+                onPressed: _create,
+              ),
+              ElevatedButton(
+                child: Text("Read"),
+                onPressed: _read,
+              ),
+              ElevatedButton(
+                child: Text("Update"),
+                onPressed: _update,
+              ),
+              ElevatedButton(
+                child: Text("Delete"),
+                onPressed: _delete,
+              ),
+            ]),
       ),
     );
   }
-}
 
-class Record {
-  final String name;
-  final int votes;
-  final firestore.DocumentReference reference;
-
-  //Redirecting Contstuctors and optional parameters
-  //https://bezkoder.com/dart-flutter-constructors/#Redirecting_Constructor
-  Record.fromSnapshot(firestore.DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data(), reference: snapshot.reference);
-
-  //Using an initializer list
-  //https://dart.dev/guides/language/language-tour#using-constructors
-
-  //How assert works
-  //https://medium.com/run-dart/dart-dartlang-introduction-advanced-dart-features-524de79456b9
-
-  //this.reference is a named optional parameter.
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['name'] != null),
-        assert(map['votes'] != null),
-        name = map['name'],
-        votes = map['votes'] {
-    //Get an objects type with .runtimeType
-    print(this.reference.runtimeType);
+  void _create() async {
+    try {
+      await firestore.collection('users').doc('testUser').set({
+        'firstName': 'test',
+        'lastName': 'user',
+      });
+      print('successful create');
+    } catch (e) {
+      print(e);
+    }
   }
 
-  @override
-  String toString() => "Record<$name:$votes>";
+  void _read() async {
+    DocumentSnapshot documentSnapshot;
+    try {
+      documentSnapshot =
+          await firestore.collection('users').doc('testUser').get();
+      print(documentSnapshot.data());
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _update() async {
+    try {
+      await firestore.collection('users').doc('testUser').update({
+        'firstName': 'testUpdated',
+      });
+      print('successful update');
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _delete() async {
+    try {
+      await firestore.collection('users').doc('testUser').delete();
+      print('successful delete');
+    } catch (e) {
+      print(e);
+    }
+  }
 }
