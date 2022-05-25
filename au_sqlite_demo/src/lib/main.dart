c1780c8786749df69b1a616b0d3b74e8f6e39ad2
robbinl <robbinl@telusplanet.net>
2022-05-25 08:04:39 -0600

    a

---
+++ au_sqlite_demo/src/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:robbinlaw/demos/aa-demo1.dart';
+import 'package:robbinlaw/demos/ab-demo2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp ({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const Demo1(),
+      //home: const Demo2(),
+    );
+  }
+}
