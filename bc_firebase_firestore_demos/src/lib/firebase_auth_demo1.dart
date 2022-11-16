b46928fee7d69b8aefdd2954ecf65ab529e0787a
robbinl <robbinl@telusplanet.net>
2022-11-16 09:52:42 -0700

    a

--- bc_firebase_firestore_demos/src/lib/firebase_auth_demo1.dart
+++ bc_firebase_firestore_demos/src/lib/firebase_auth_demo1.dart
@@ -1,12 +1,13 @@
 //https://firebase.flutter.dev/docs/auth/usage/
 
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:robbinlaw/themes/theme.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage ({Key? key}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
   @override
   MyHomePageState createState() => MyHomePageState();
 }
@@ -36,131 +37,129 @@ class MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("Firebase Auth Demo1"),
       ),
-      body: Container(
-        child: Form(
-          key: formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextFormField(
-                  controller: textEditingController1,
-                  onChanged: (value) {
-                    //print(value);
-                  },
-                  onFieldSubmitted: (text) {
-                    if (formKey.currentState!.validate()) {}
-                    print('Submitted Email Text = $text');
-                  },
-                  validator: (input) {
-                    return input!.contains('@') ? null : 'hey it must include @';
-                  },
-                  onSaved: (input) {
-                    print('onSaved email = $input');
-                    _email = input.toString();
-                  },
-                  cursorColor: colorScheme.onPrimary,
-                  maxLength: 30,
-                  decoration: InputDecoration(
-                    //The border property is what makes a outlined
-                    //textformfield instead of a filled one.
-                    border: const OutlineInputBorder(),
-                    icon: Icon(
-                      Icons.email,
-                      color: colorScheme.onBackground,
-                    ),
-                    labelText: 'email',
-                    helperText: 'must include @',
-                    suffixIcon: Icon(
-                      Icons.check_circle,
-                      color: colorScheme.onPrimary,
-                    ),
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: textEditingController1,
+                onChanged: (value) {
+                  //print(value);
+                },
+                onFieldSubmitted: (text) {
+                  if (formKey.currentState!.validate()) {}
+                  print('Submitted Email Text = $text');
+                },
+                validator: (input) {
+                  return input!.contains('@') ? null : 'hey it must include @';
+                },
+                onSaved: (input) {
+                  print('onSaved email = $input');
+                  _email = input.toString();
+                },
+                //cursorColor: colorScheme.onPrimary,
+                maxLength: 30,
+                decoration: const InputDecoration(
+                  //The border property is what makes a outlined
+                  //textformfield instead of a filled one.
+                  border: OutlineInputBorder(),
+                  icon: Icon(
+                    Icons.email,
+                    //color: colorScheme.onBackground,
+                  ),
+                  labelText: 'email',
+                  helperText: 'must include @',
+                  suffixIcon: Icon(
+                    Icons.check_circle,
+                    //color: colorScheme.onPrimary,
                   ),
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  controller: textEditingController2,
-                  onChanged: (value) {
-                    //print(value);
-                  },
-                  onFieldSubmitted: (text) {
-                    if (formKey.currentState!.validate()) {}
-                    //print('Submitted Password Text = $text');
-                  },
-                  validator: (input) {
-                    return input!.length < 6 ? 'min 6 chars' : null;
-                  },
-                  onSaved: (input) {
-                    _password = input.toString();
-                    print('onSaved password = $input');
-                  },
-                  //obscureText: true,
-                  cursorColor: colorScheme.onPrimary,
-                  maxLength: 20,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    icon: Icon(
-                      Icons.emoji_emotions,
-                      color: colorScheme.onBackground,
-                    ),
-                    labelText: 'password',
-                    helperText: 'min 6, max 20',
-                    suffixIcon: Icon(
-                      Icons.check_circle,
-                      color: colorScheme.onPrimary,
-                    ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: textEditingController2,
+                onChanged: (value) {
+                  //print(value);
+                },
+                onFieldSubmitted: (text) {
+                  if (formKey.currentState!.validate()) {}
+                  //print('Submitted Password Text = $text');
+                },
+                validator: (input) {
+                  return input!.length < 6 ? 'min 6 chars' : null;
+                },
+                onSaved: (input) {
+                  _password = input.toString();
+                  print('onSaved password = $input');
+                },
+                //obscureText: true,
+                //cursorColor: colorScheme.onPrimary,
+                maxLength: 20,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  icon: Icon(
+                    Icons.emoji_emotions,
+                    //color: colorScheme.onBackground,
+                  ),
+                  labelText: 'password',
+                  helperText: 'min 6, max 20',
+                  suffixIcon: Icon(
+                    Icons.check_circle,
+                    //color: colorScheme.onPrimary,
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      child: const Text("Sign Up"),
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          formKey.currentState!.save();
-                          //textEditingController1.clear();
-                          //textEditingController2.clear();
-                          setState(() {});
-                        }
-                        _signUp(_email, _password);
-                        //_email = '';
-                        //_password = '';
-                      },
-                    ),
-                    ElevatedButton(
-                      child: const Text("Log In"),
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          formKey.currentState!.save();
-                          //textEditingController1.clear();
-                          //textEditingController2.clear();
-                          setState(() {});
-                        }
-                        _logIn(_email, _password);
-                        //_email = '';
-                        //_password = '';
-                      },
-                    ),
-                    ElevatedButton(
-                      child: const Text("Log Out"),
-                      onPressed: () {
-                        _logOut();
-                        _email = '';
-                        _password = '';
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    child: const Text("Sign Up"),
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        formKey.currentState!.save();
+                        //textEditingController1.clear();
+                        //textEditingController2.clear();
+                        setState(() {});
+                      }
+                      _signUp(_email, _password);
+                      //_email = '';
+                      //_password = '';
+                    },
+                  ),
+                  ElevatedButton(
+                    child: const Text("Log In"),
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        formKey.currentState!.save();
+                        //textEditingController1.clear();
+                        //textEditingController2.clear();
+                        setState(() {});
+                      }
+                      _logIn(_email, _password);
+                      //_email = '';
+                      //_password = '';
+                    },
+                  ),
+                  ElevatedButton(
+                    child: const Text("Log Out"),
+                    onPressed: () {
+                      _logOut();
+                      _email = '';
+                      _password = '';
+                    },
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
