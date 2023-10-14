0c23cbb2e88d45c8e8b7edc3993bc517f892c789
robbinl <robbinl@telusplanet.net>
2023-10-14 17:22:56 -0600

    a

--- ay_gps_WEATHERAPP2/src23/lib/main.dart
+++ ay_gps_WEATHERAPP2/src23/lib/main.dart
@@ -1,11 +1,4 @@
-//VERY IMPORTANT!!!
-//To add the geolocator to your Flutter application
-//read the install instructions in
-//the "docs" folder of this project.
-//In there, are some Android and iOS specifics
-//that are required for the geolocator to work correctly.
-
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, unused_import
 
 import 'package:flutter/material.dart';
 //import 'package:get/get.dart';
@@ -13,12 +6,18 @@ import 'package:flutter/material.dart';
 import 'package:robbinlaw/views/loading-view1.dart';
 import 'package:robbinlaw/views/loading-view2.dart';
 
+//VERY IMPORTANT!!!
+//To add the geolocator to your Flutter application
+//read the install instructions in
+//the "docs" folder of this project.
+//In there, are some Android and iOS specifics
+//that are required for the geolocator to work correctly.
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  //const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
